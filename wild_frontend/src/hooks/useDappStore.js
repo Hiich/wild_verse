9f81a1592329a0cd914d8a1874af31bc644e4396
unknown <hicham.benhima1@gmail.com>
2022-10-20 14:24:22 +0200

    go live

--- wild_frontend/src/hooks/useDappStore.js
+++ wild_frontend/src/hooks/useDappStore.js
@@ -4,22 +4,31 @@ import { parseEther } from 'ethers/lib/utils'
 import abi from '../config/abi.json'
 import config from '../config/config.json'
 
-const provider = new ethers.providers.Web3Provider(window.ethereum)
 
 const useDappStore = create((set) => ({
     account: null,
     smartContract: undefined,
     mint: (count) => set(async (state) => {
-        const costInWei = 1000000000000000 * count
-        await state.smartContract?.mint(state.account, count, { value: costInWei })
+        if (typeof window.ethereum === 'undefined') {
+            console.log('MetaMask not installed!');
+        } else {
+            const costInWei = 1000000000000000 * count
+            await state.smartContract?.mint(state.account, count, { value: costInWei })
+        }
     }),
     connect: async (state) => {
-        console.log("Connecting")
-        const accounts = await provider.send("eth_requestAccounts", []);
-        const account = accounts[0]
-        const signer = provider.getSigner()
-        const smartContract = new ethers.Contract(config.contract_address, abi, signer)
-        set({ account: account, smartContract: smartContract })
+        if (typeof window.ethereum === 'undefined') {
+            console.log('MetaMask not installed!');
+        }
+        else {
+            console.log("Connecting")
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            const accounts = await provider.send("eth_requestAccounts", []);
+            const account = accounts[0]
+            const signer = provider.getSigner()
+            const smartContract = new ethers.Contract(config.contract_address, abi, signer)
+            set({ account: account, smartContract: smartContract })
+        }
     }
 }))
 
